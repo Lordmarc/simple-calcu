5b9b911ee884bfb94322d6c22f8e73d64ec93749
lordmarc <matabanglordmarcsiega@gmail.com>
2025-01-10 12:56:41 +0800

    fix calcu

---
+++ index.js
@@ -0,0 +1,23 @@
+const screen = document.getElementById('screen');
+      
+     
+
+document.querySelectorAll('.btn').forEach(button =>{
+  button.addEventListener('click',function(){
+    const value = button.getAttribute('data-value');
+
+    if(value === "C"){
+      screen.value = "";
+      previous.value = "";
+    }else if(value === "DEL"){
+      screen.value = screen.value.slice(0, -1);
+    } if(value === "="){
+     
+      screen.value = eval(screen.value.replace(/÷/g, "/").replace(/X/g, "*"));
+    }
+    else{
+      screen.value += value;
+     
+    }
+  })
+})
