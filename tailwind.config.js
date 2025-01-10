6058e2e4aca2bcbfcdb6401f82b6997757aac26c
lordmarc <matabanglordmarcsiega@gmail.com>
2025-01-10 13:35:24 +0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"
-  ],
+  content: ["./index.html", "./index.js", "./src/**/*.{html,js}"],
+
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
