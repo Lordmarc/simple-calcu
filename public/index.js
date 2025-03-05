2f84f1c025af91ff34541cc624885c2ea1610021
lordmarc <matabanglordmarcsiega@gmail.com>
2025-03-05 17:05:09 +0800

    new

--- public/index.js
+++ public/index.js
@@ -9,7 +9,8 @@ document.querySelectorAll(".btn").forEach((button) => {
     } else if (value === "DEL") {
       screen.value = screen.value.slice(0, -1);
     } else if (value === "=") {
-      screen.value = eval(screen.value.replace(/÷/g, "/").replace(/X/g, "*"));
+      screen.value = "Kupal kaba boss?";
+      //screen.value = eval(screen.value.replace(/÷/g, "/").replace(/X/g, "*"));
     } else {
       screen.value += value;
     }
